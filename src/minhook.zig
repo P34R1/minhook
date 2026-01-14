c0bb80c51e06f46e3152693e89aa5db00efd1966
pearl <undeadgamer279@gmail.com>
2026-01-13 22:17:21 -0500

    comments

--- src/minhook.zig
+++ src/minhook.zig
@@ -1,16 +1,16 @@
 const LPVOID = *anyopaque;
+const MH_ALL_HOOKS: ?LPVOID = null;
 
 extern fn MH_Initialize() callconv(.winapi) STATUS;
 extern fn MH_Uninitialize() callconv(.winapi) STATUS;
-
 extern fn MH_CreateHook(pTarget: LPVOID, pDetour: LPVOID, ppOriginal: ?*LPVOID) callconv(.winapi) STATUS;
 extern fn MH_RemoveHook(pTarget: ?LPVOID) callconv(.winapi) STATUS;
-
 extern fn MH_EnableHook(pTarget: ?LPVOID) callconv(.winapi) STATUS;
 extern fn MH_DisableHook(pTarget: ?LPVOID) callconv(.winapi) STATUS;
 
+/// MinHook Error Codes.
 pub const Error = error{ AlreadyInitialized, NotInitialized, AlreadyCreated, NotCreated, Enabled, Disabled, NotExecutable, UnsupportedFunction, MemoryAlloc, MemoryProtect, ModuleNotFound, FunctionNotFound, MutexFailure };
-pub const STATUS = enum(c_int) {
+const STATUS = enum(c_int) {
     OK = 0,
     ERROR_ALREADY_INITIALIZED,
     ERROR_NOT_INITIALIZED,
@@ -48,14 +48,25 @@ pub const STATUS = enum(c_int) {
     }
 };
 
+/// Initialize the MinHook library. You must call this function EXACTLY ONCE
+/// at the beginning of your program.
 pub fn init() Error!void {
     return MH_Initialize().toError();
 }
 
+/// Uninitialize the MinHook library. You must call this function EXACTLY
+/// ONCE at the end of your program.
 pub fn deinit() Error!void {
     return MH_Uninitialize().toError();
 }
 
+/// Creates a hook for the specified target function, in disabled state.
+///
+/// `target` is a pointer to the target function, which will be overridden by the detour function.
+///
+/// `detour` is the detour function, which will override the target function.
+///
+/// Returns a pointer to the trampoline function, which will be used to call the original target function.
 pub fn create(target: *const anyopaque, detour: anytype) Error!*const @TypeOf(detour) {
     if (@typeInfo(@TypeOf(detour)) != .@"fn")
         @compileError("detour must be a function");
@@ -66,15 +77,17 @@ pub fn create(target: *const anyopaque, detour: anytype) Error!*const @TypeOf(de
     return original;
 }
 
-const MH_ALL_HOOKS: ?LPVOID = null;
+/// Removes all already created hooks.
 pub fn removeAll() Error!void {
     return MH_RemoveHook(MH_ALL_HOOKS).toError();
 }
 
+/// Enables all already created hooks.
 pub fn enableAll() Error!void {
     return MH_EnableHook(MH_ALL_HOOKS).toError();
 }
 
+/// Disables all already created hooks.
 pub fn disableAll() Error!void {
     return MH_DisableHook(MH_ALL_HOOKS).toError();
 }
